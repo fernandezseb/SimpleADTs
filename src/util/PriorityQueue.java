fc84b1005e4fd6492cfcf7378e65f61a7dccdb9c
Sebastiaan Fernandez <sebastiaan.fernandez.w1944@student.hogent.be>
2017-03-30 01:06:44 +0200

    Translate and improve comments and remove unneeded if statement

--- src/util/PriorityQueue.java
+++ src/util/PriorityQueue.java
@@ -80,23 +80,23 @@ public class PriorityQueue<E extends Comparable<E>> {
     private void bubbleUp(int index) {
         int prio = items.get(index).getPriority();
 
-        // current is eerst het element dat we beschouwen
+        // current the first element
         Item<E> current = items.get(index);
+        // The index of the next item (parent) to check
         int i = (int) Math.floor(index / 2);
 
+        // Make sure we don't go into the 0 element or negative elements
         while (i > 0) {
-            // check if it fits in the array
-            if (i <  1) {
-                break;
-            }
 
-            // Vergelijken met de parent
+            // Compare the priority with the parent
             if(prio < items.get(i).getPriority()) {
-                // verwisselen met de parent
+                // Switch current value with the parent's
                 exchangeData(current, items.get(i));
             }
 
+            // Set the current to the parent
             current = items.get(i);
+            // Set the new index of the supposed parent
             i = (int) Math.floor(i / 2);
         }
     }
@@ -104,24 +104,35 @@ public class PriorityQueue<E extends Comparable<E>> {
     private void bubbleDown(int current) {
 
 
+            /*
+             * Calculate the left and right indices
+             */
             int leftIndex = current*2;
             int rightIndex = current*2 + 1;
 
+            /*
+             * Get the left and right child priorities, set to maximum value in case of an empty child,
+             * so that in won't try to switch later in the code
+             */
             int leftprio = leftIndex  < items.size() ? items.get(leftIndex).getPriority() : Integer.MAX_VALUE;
             int rightprio = rightIndex < items.size() ? items.get(rightIndex).getPriority() : Integer.MAX_VALUE;
 
+            // If the left child has higher priority, switch.
             if(leftprio < items.get(current).getPriority()) {
                 exchangeData(items.get(current), items.get(leftIndex));
             }
 
+            // If the right child has higher priority, switch.
             if(rightprio < items.get(current).getPriority()) {
                 exchangeData(items.get(current), items.get(rightIndex));
             }
 
+            // If there is a left child, run recursive for the left child
             if(leftIndex < items.size()) {
                 bubbleDown(leftIndex);
             }
 
+            // If there is a right child, run recursive for the right child
             if(rightIndex < items.size()) {
                 bubbleDown(rightIndex);
             }
@@ -130,7 +141,6 @@ public class PriorityQueue<E extends Comparable<E>> {
 
 
     private void exchangeData(Item<E> l, Item<E> r) {
-        System.out.println("Exchanging " + l.getPriority() + " with " + r.getPriority());
         E value = l.getData();
         l.setData(r.getData());
         r.setData(value);
